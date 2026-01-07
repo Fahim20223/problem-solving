1979a9d917a979efde7c18b0ff3abfaff3a73a44
Fahim2023 <fahimshahrier2023@gmail.com>
2026-01-07 23:24:51 +0600

    added problem solving page

---
+++ problem-1.js
@@ -0,0 +1,16 @@
+// Problem 1: Reverse a String
+// Write a function that takes a string and returns it reversed.
+
+// Example:
+
+// Input: "hello"
+// Output: "olleh"
+
+// Solve :
+
+const greeting = "hello";
+
+const splits = greeting.split("");
+const reversing = splits.reverse().join("");
+// console.log(spliting);
+console.log(reversing);
